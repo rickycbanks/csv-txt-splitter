cb43e338a8fa1ef2461a8505de6e095e029236d7
Richard Banks <richardcbanks@zoho.com>
2022-06-18 13:49:42 -0400

    prep for main launch - switching computers
    Switching os's backing up new dev. not functional

--- Source/csvsplitter.py
+++ Source/csvsplitter.py
@@ -1,15 +1,79 @@
-import csv
-import os
-import sys
+#import csv
+#import os
+#import sys
 import worker.split as isp
 import PySimpleGUI as sg
 
-def complete():
-    sg.Popup('Complete')
-    main_diag()
+# Define Initial Theme and Get themes
+theme = 'lightGrey1'
+
+## Set Layout of Main Window
+main_layout = [[sg.Text('Filename'),sg.Input(key='fname'), sg.FileBrowse()],
+    [sg.Text('Destination Folder: '),sg.Input(key='fold'), sg.FolderBrowse()],
+    [sg.Text('Rows per split: '),sg.Input('100000', key='split', )],
+    [sg.Text('Suffix: '),sg.Input(key='suff')],
+    [sg.OK('Split'),sg.Exit('Exit')] ]
+
+
+## Layout of popup window
+popup_layout = [sg.Text('Splitting Files'),
+    [sg.ProgressBar('?',)]]
+
+
+sg.change_look_and_feel(theme)
+
+## Draw main windows
+window = sg.Window('CSV | TXT | File Splitter',layout=main_layout)
+
+## Event Loop Main Window
+while True:  # Event Loop
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Split':
+        runsplit=isp.split_csv(values['fname'], values['fold'], values['suff'], int(values['split']))
+        if runsplit:
+            sg.popup_ok(title='Success')
+
+window.close()
+
+'''class diag:
+    def __init__(self):
+        self.layout = []
+        window = None
+
+    def main(self):
+        sg.change_look_and_feel('lightGrey1')
+        self.layout = [[sg.Text('Filename'),sg.Input(key='fname'), sg.FileBrowse()],
+            [sg.Text('Destination Folder: '),sg.Input(key='fold'), sg.FolderBrowse()],
+            [sg.Text('Rows per split: '),sg.Input('100000', key='split', )],
+            [sg.Text('Suffix: '),sg.Input(key='suff')],
+            [sg.OK('Split'),sg.Exit('Exit')] ]
+        print('main')
+        return self.layout
+
+    def main(self):
+        self.layout = [[sg.Text('Splitting the file.')]
+            [sg.l]
+        ]
+        
+
+    def render(self):
+        print('render')
+        lay = self.layout
+        window = sg.Window('Ricky\'s Splitter', lay) 
+        event, values = window.read()
+
+isp.split_csv(SOURCE_FILEPATH, DEST_PATH, FILENAME_SUFFIX, ROW_LIMIT)'''
 
 
 
+
+
+
+
+'''
 #Actual Dialog
 def main_diag():
     # Layout Window - look and feel
@@ -28,33 +92,16 @@ def main_diag():
     FILENAME_PREFIX = values['pref']
     ROW_LIMIT = int(values['split'])
 
+'''
+
+
+
+
+'''def complete():
+    sg.Popup('Complete')
+    main_diag()
+
     isp.split_csv(SOURCE_FILEPATH, DEST_PATH, FILENAME_PREFIX, ROW_LIMIT)
 
     complete()
-
-main_diag()
-
-'''
-import PySimpleGUI as sg
-
-sg.change_look_and_feel('Dark Blue 3')  # please make your windows colorful
-
-layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(12,1), key='_OUTPUT_')],
-          [sg.Input(key='_IN_')],
-          [sg.Button('Show'), sg.Button('Exit')]]
-
-window = sg.Window('Window Title', layout)
-
-while True:  # Event Loop
-    event, values = window.read()       # can also be written as event, values = window()
-    print(event, values)
-    if event is None or event == 'Exit':
-        break
-    if event == 'Show':
-        # change the "output" element to be the value of "input" element
-        window['_OUTPUT_'].update(values['_IN_'])
-        # above line can also be written without the update specified
-        window['_OUTPUT_'](values['_IN_'])
-
-window.close()
 '''
